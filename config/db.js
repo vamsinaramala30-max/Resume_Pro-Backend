166c211b4e117728d1b5e09598d0c66957b05c84
Vamsi Krishna <vamsinaramala30@gmail.com>
2026-05-28 15:30:44 +0530

    db.js

--- config/db.js
+++ config/db.js
@@ -2,10 +2,17 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
+    // Debug check
+    console.log("MONGO_URI:", process.env.MONGO_URI);
+
+    // Connect MongoDB
     await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB Connected ✅");
-  } catch (err) {
-    console.log("MongoDB Error ❌", err.message);
+
+    console.log("✅ MongoDB Connected Successfully");
+  } catch (error) {
+    console.error("❌ MongoDB Connection Error:", error.message);
+
+    // Stop server if DB fails
     process.exit(1);
   }
 };
